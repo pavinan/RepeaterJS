a54147c05f1dd5c91ad221f06a5d0b29653e9c43
Pavinan <pavinan@users.noreply.github.com>
2016-05-09 17:13:06 +0530

    intellisense

---
+++ RepeaterJS/RepeaterJS/src/Repeater.intellisense.js
@@ -0,0 +1,10 @@
+﻿
+intellisense.annotate(jQuery, {
+    "Repeater": function () {
+        ///<signature>
+        /// <summary>Repeater extension for repeating UI</summary>
+        /// <param name="options" type="PlainObject"></param>
+        ///</signature>
+    },
+
+});

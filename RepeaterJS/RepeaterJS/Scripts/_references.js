adb6005cc7fde5c5a83e501213f6a635d9464501
Pavinan <pavinan@users.noreply.github.com>
2016-05-09 12:54:19 +0530

    Basic code

---
+++ RepeaterJS/RepeaterJS/Scripts/_references.js
@@ -0,0 +1,3 @@
+﻿/// <autosync enabled="true" />
+/// <reference path="jquery-2.2.3.js" />
+/// <reference path="../src/repeater.js" />
---
+++ RepeaterJS/RepeaterJS/src/Repeater.js
@@ -0,0 +1,33 @@
+﻿(function ($) {
+
+    function Repeater(options) {
+
+        var settings = $.extend({}, options);
+
+        var itemTemplate = $($(this).find('[data-template]').first().html());
+
+        $.each(settings.data, function (key, value) {
+
+        });
+    }
+
+    $.fn.Repeater = Repeater;
+
+})(jQuery);
+
+
+/*
+
+options = {
+
+    data: [],
+
+    onItemCreating: function(sender, eventArgs) : true|false,
+
+    itemClicked: function(sender,eventArgs) : undefined,
+
+
+
+}
+
+*/
